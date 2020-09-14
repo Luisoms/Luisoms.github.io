a1a6b99a5e20eae7407e15efd59264c0e26cd2aa
Luisoms <lomsloms13@gmail.com>
2020-09-14 12:43:54 -0400

    Updates

---
+++ precache-manifest.7be08f4f5a4b8f544398334c3bdf8c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1955f1903c84fa68e4c0e608d08007da",
+    "url": "/react-practices/index.html"
+  },
+  {
+    "revision": "cd4ee1dfd238931dcc58",
+    "url": "/react-practices/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5567f761d182c6018d6",
+    "url": "/react-practices/static/js/2.96b30471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-practices/static/js/2.96b30471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4ee1dfd238931dcc58",
+    "url": "/react-practices/static/js/main.f6753a88.chunk.js"
+  },
+  {
+    "revision": "f9d3097f6eec65044356",
+    "url": "/react-practices/static/js/runtime-main.a9466f74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practices/static/media/logo.5d5d9eef.svg"
+  }
+]);
